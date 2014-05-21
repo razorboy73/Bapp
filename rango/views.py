e85256397b931881cccefcaa9353bec8169d78fe
razorboy73 <josh.kerbel@femme-o-nomics.com>
2014-05-21 09:17:32 -0700

    Fixed category view bug

--- rango/views.py
+++ rango/views.py
@@ -6,6 +6,7 @@ from rango.forms import CategoryForm
 from rango.forms import  PageForm, UserForm, UserProfileForm
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 
 def encode_url(str):
     return str.replace(' ', '_')
@@ -29,6 +30,29 @@ def index(request):
     #got a list of page objects and added them to the context view
     page_list = Page.objects.order_by('-views')[:5]
     context_dict ['pages'] = page_list
+    response = render_to_response('rango/index.html', context_dict, context)
+    visits = int(request.COOKIES.get('visits', '0'))
+    # Does the cookie last_visit exist?
+    if 'last_visit' in request.COOKIES:
+        # Yes it does! Get the cookie's value.
+        last_visit = request.COOKIES['last_visit']
+        # Cast the value to a Python date/time object.
+        last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+        # If it's been more than a day since the last visit...
+        if (datetime.now() - last_visit_time).seconds > 10:
+            # ...reassign the value of the cookie to +1 of what it was before...
+            response.set_cookie('visits', visits+1)
+            # ...and update the last visit cookie, too.
+            response.set_cookie('last_visit', datetime.now())
+    else:
+        # Cookie last_visit doesn't exist, so create it to the current date/time.
+        response.set_cookie('last_visit', datetime.now())
+
+    # Return response back to the user, updating any cookies that need changed.
+    return response
+    #### END NEW CODE ####
+
+
     # The following two lines are new.
     # We loop through each category returned, and create a URL attribute.
     # This attribute stores an encoded URL (e.g. spaces replaced with underscores).
